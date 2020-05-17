cc199fb93cfdf801b2412084f957c559e2180a63
Xuqi <xwang193@ucsc.edu>
2020-05-17 15:30:25 -0700

    9th

---
+++ prefabs/Water.js
@@ -0,0 +1,16 @@
+class Water extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);  
+    }
+    update() {
+        this.x -= game.settings.spaceshipSpeed;
+        if (this.x <= 0-this.width) {
+            this.reset();
+            //this.y = Math.random() * 300 + 100;
+        }
+    }
+    reset() {
+        this.x = game.config.width*4;
+    }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,27 +2,26 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
-
     preload() {
         // load images/tile sprites
         this.load.image('Stone', './assets/Stone.png');
         this.load.image('Gold', './assets/Gold.png');
         this.load.image('Back', './assets/Back.png');
+        this.load.image('Water', './assets/Water.jpg');
         this.load.image('RabbitDie', './assets/rabbitdie.png');
         this.load.spritesheet('Rabbit', './assets/rabbitAtlas.png', {frameWidth: 60, frameHeight: 110, });
     }
-
     isPlaying = false;
-
     create() {
         // place tile sprite
         this.Back = this.add.tileSprite(0, 0, game.config.width, game.config.height + 250, 'Back').setScale(1, 0.7).setOrigin(0, 0); 
         game.music.play();
-        
-
-        // green UI background
-        //this.add.rectangle(0, 12, game.config.width, 50, 0x00FF00).setOrigin(0, 0);
-  
+        // add spaceships (x3)
+        this.rock01 = new Rock(this, game.config.width + 192, Math.random() * 100 + 100, 'Stone', 0).setScale(0.25, 0.25).setOrigin(0,0);
+        this.rock02 = new Rock(this, game.config.width + 96, Math.random() * 200 + 100, 'Stone', 0).setScale(0.25, 0.25).setOrigin(0,0);
+        this.rock03 = new Rock(this, game.config.width, Math.random() * 300 + 100, 'Stone', 0).setScale(0.25, 0.25).setOrigin(0,0);
+        this.water = new Water(this, game.config.width*3, 0, 'Water', 0).setScale(1, 1.6).setOrigin(0,0);
+        this.gold = new Gold(this, game.config.width, Math.random() * 300 + 100, 'Gold', 0, 50, 4).setOrigin(0,0);
         // add rocket (p1)
         this.rabbit = new Rabbit(this, 20, game.config.height/2, 'Rabbit').setScale(0.5, 0.5).setOrigin(0, 0);
         this.anims.create({
@@ -32,20 +31,11 @@ class Play extends Phaser.Scene {
             frameRate: 6
         });
         this.rabbit.play('RabbitAnimas');
-        
-
-        // add spaceships (x3)
-        this.rock01 = new Rock(this, game.config.width + 192, Math.random() * 100 + 100, 'Stone', 0).setScale(0.25, 0.25).setOrigin(0,0);
-        this.rock02 = new Rock(this, game.config.width + 96, Math.random() * 200 + 100, 'Stone', 0).setScale(0.25, 0.25).setOrigin(0,0);
-        this.rock03 = new Rock(this, game.config.width, Math.random() * 300 + 100, 'Stone', 0).setScale(0.25, 0.25).setOrigin(0,0);
-        this.gold = new Gold(this, game.config.width, Math.random() * 300 + 100, 'Gold', 0, 50, 4).setOrigin(0,0);
-
         // white rectangle borders
         this.add.rectangle(0, 0, game.config.width, 12, 0x000000).setOrigin(0, 0);// up
         this.add.rectangle(0, game.config.height - 12, game.config.width, 12, 0x000000).setOrigin(0, 0); // down
         this.add.rectangle(0, 0, 12, game.config.height, 0x000000).setOrigin(0, 0); // left
         this.add.rectangle(game.config.width - 12, 0, 12, game.config.height, 0x000000).setOrigin(0, 0); // right
-
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -53,18 +43,15 @@ class Play extends Phaser.Scene {
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
         // animation config
         this.anims.create({
             key: 'explode',
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
             frameRate: 30
         });
-
         // player 1 score
         this.p1Score = 0;
         this.hScore = highScore;
-
         // score display UI
         let scoreConfig = {
             fontFamily: 'Courier',
@@ -83,6 +70,7 @@ class Play extends Phaser.Scene {
         // game over flag
         this.gameOver = false;
         scoreConfig.fixedWidth = 0;
+        this.Scale = 0;
     }
     update() {
         // check key input for restart / menu
@@ -120,23 +108,56 @@ class Play extends Phaser.Scene {
             this.rock02.update();
             this.rock03.update();
             this.gold.update();
+            this.water.update();
         }             
+        if (!this.gameOver && Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            if(this.Scale == 0){
+                this.rabbit.setScale(1,1);
+                this.Scale = 1;
+            }else if(this.Scale == 1){
+                this.rabbit.setScale(0.5,0.5);
+                this.Scale = 0;
+            }
+        }
         // check collisions
         if(this.checkCollision(this.rabbit, this.rock03)) {
-            this.rabbit.destroy();
-            this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
-            this.gameOver = true;
+            if(this.Scale == 0){
+                this.rabbit.destroy();
+                this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
+                this.gameOver = true;
+            }else{
+                this.rock03.reset();
+                this.rock03.y = Math.random() * 300 + 100;
+            }
         }
         if (this.checkCollision(this.rabbit, this.rock02)) {
-            this.rabbit.destroy();
-            this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
-            this.gameOver = true;
+            if(this.Scale == 0){
+                this.rabbit.destroy();
+                this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
+                this.gameOver = true;
+            }else{
+                this.rock02.reset();
+                this.rock02.y = Math.random() * 300 + 100;
+            }
         }
         if (this.checkCollision(this.rabbit, this.rock01)) {
-            this.rabbit.destroy();
-            this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
-            this.gameOver = true;
+            if(this.Scale == 0){
+                this.rabbit.destroy();
+                this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
+                this.gameOver = true;
+            }else{
+                this.rock01.reset();
+                this.rock01.y = Math.random() * 300 + 100;
+            }
         }
+        if (this.checkCollisionWater(this.rabbit, this.water)){
+            if(this.Scale == 0){
+                this.rabbit.destroy();
+                this.add.sprite(this.rabbit.x, this.rabbit.y + 40, 'RabbitDie').setScale(0.08, 0.08);
+                this.gameOver = true;
+            }
+        }
+        
         if (this.checkCollisionGold(this.rabbit, this.gold)) {
             this.p1Score += this.gold.points;
             this.scoreLeft.text = this.p1Score;  
@@ -149,13 +170,45 @@ class Play extends Phaser.Scene {
         }
     }
     checkCollision(rabbit, rock) {
-        if (rabbit.x < rock.x + rock.width*0.25 && 
-            rabbit.x + rabbit.width*0.25 > rock.x && 
-            rabbit.y < rock.y + rock.height*0.15 &&
-            rabbit.height*0.25 + rabbit.y > rock.y) {
-                return true;
-        } else {
-            return false;
+        if  (!this.Scale == 1){
+            if (rabbit.x < rock.x + rock.width*0.25 && 
+                rabbit.x + rabbit.width*0.25 > rock.x && 
+                rabbit.y < rock.y + rock.height*0.15 &&
+                rabbit.height*0.25 + rabbit.y > rock.y) {
+                    return true;
+            } else {
+                return false;
+            }
+        }else{
+            if (rabbit.x < rock.x + rock.width*0.5 && 
+                rabbit.x + rabbit.width*0.5 > rock.x && 
+                rabbit.y + 40 < rock.y + rock.height*0.5 &&
+                rabbit.height*0.5 + rabbit.y + 20 > rock.y) {
+                    return true;
+            } else {
+                return false;
+            }   
+        }
+    }
+    checkCollisionWater(rabbit, water) {
+        if  (this.Scale == 0){
+            if (rabbit.x < water.x + water.width && 
+                rabbit.x + rabbit.width - 40 > water.x && 
+                rabbit.y < water.y + water.height &&
+                rabbit.height + rabbit.y > water.y) {
+                    return true;
+            } else {
+                return false;
+            }
+        }else{
+            if (rabbit.x < water.x + water.width*0.5 && 
+                rabbit.x + rabbit.width*0.5 > water.x && 
+                rabbit.y + 40 < water.y + water.height*0.5 &&
+                rabbit.height*0.5 + rabbit.y + 20 > water.y) {
+                    return true;
+            } else {
+                return false;
+            }   
         }
     }
     checkCollisionGold(rabbit, rock) {
